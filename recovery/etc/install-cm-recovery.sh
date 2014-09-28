b3a6e2d50de34bfb163a28cabae50dd9bac9ad15
JustArchi <JustArchi@JustArchi.net>
2014-09-28 05:16:15 +0200

    Refresh build to fix broken setup wizard https://github.com/ArchiDroid/android_vendor_cm/commit/0df7803c0ab0a0fe1d58c0a2b7ea59235b76f77e

--- recovery/etc/install-cm-recovery.sh
+++ recovery/etc/install-cm-recovery.sh
@@ -1,11 +1,11 @@
 #!/system/bin/sh
 if [ -f /system/etc/recovery-transform.sh ]; then
-  exec sh /system/etc/recovery-transform.sh 7993344 4e2444964e381aa7823812a7a49f33a523c6a054 5087232 022986cb70001d734ed6f18225297f345e324350
+  exec sh /system/etc/recovery-transform.sh 7993344 20c6aa5e8e3f44b35fcc1447ce5670e85e1128be 5087232 d07455495c3f695f7eecd811375b18a324a60756
 fi
 
-if ! applypatch -c EMMC:/dev/block/mmcblk0p6:7993344:4e2444964e381aa7823812a7a49f33a523c6a054; then
+if ! applypatch -c EMMC:/dev/block/mmcblk0p6:7993344:20c6aa5e8e3f44b35fcc1447ce5670e85e1128be; then
   log -t recovery "Installing new recovery image"
-  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/mmcblk0p5:5087232:022986cb70001d734ed6f18225297f345e324350 EMMC:/dev/block/mmcblk0p6 4e2444964e381aa7823812a7a49f33a523c6a054 7993344 022986cb70001d734ed6f18225297f345e324350:/system/recovery-from-boot.p
+  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/mmcblk0p5:5087232:d07455495c3f695f7eecd811375b18a324a60756 EMMC:/dev/block/mmcblk0p6 20c6aa5e8e3f44b35fcc1447ce5670e85e1128be 7993344 d07455495c3f695f7eecd811375b18a324a60756:/system/recovery-from-boot.p
 else
   log -t recovery "Recovery image already installed"
 fi
