88fe7b06f5f07ec365d36bcc957f213ce1b29fca
Łukasz Domeradzki <JustArchi@JustArchi.net>
2014-12-24 02:09:40 +0100

    Updated sources, ARCHI BROUGHT YOU WORKING HWCOMPOSER FOR CHRISTMAS

--- recovery/etc/install-cm-recovery.sh
+++ recovery/etc/install-cm-recovery.sh
@@ -1,11 +1,11 @@
 #!/system/bin/sh
 if [ -f /system/etc/recovery-transform.sh ]; then
-  exec sh /system/etc/recovery-transform.sh 7845888 8b957cc753bf2de7effa693b6a712e962ea40182 5087232 a3ddaf51d4537aba2387e5957597f3af0780405c
+  exec sh /system/etc/recovery-transform.sh 8228864 28c7e996ecd9aa07d4e699bdeebfd141ed38769c 5470208 274a8df445b9006adee6b7c1fab244c2b67409fc
 fi
 
-if ! applypatch -c EMMC:/dev/block/mmcblk0p6:7845888:8b957cc753bf2de7effa693b6a712e962ea40182; then
+if ! applypatch -c EMMC:/dev/block/mmcblk0p6:8228864:28c7e996ecd9aa07d4e699bdeebfd141ed38769c; then
   log -t recovery "Installing new recovery image"
-  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/mmcblk0p5:5087232:a3ddaf51d4537aba2387e5957597f3af0780405c EMMC:/dev/block/mmcblk0p6 8b957cc753bf2de7effa693b6a712e962ea40182 7845888 a3ddaf51d4537aba2387e5957597f3af0780405c:/system/recovery-from-boot.p
+  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/mmcblk0p5:5470208:274a8df445b9006adee6b7c1fab244c2b67409fc EMMC:/dev/block/mmcblk0p6 28c7e996ecd9aa07d4e699bdeebfd141ed38769c 8228864 274a8df445b9006adee6b7c1fab244c2b67409fc:/system/recovery-from-boot.p
 else
   log -t recovery "Recovery image already installed"
 fi
