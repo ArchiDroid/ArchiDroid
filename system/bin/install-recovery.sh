bc220876918cfb1129610501ad1cc3891d65d8c5
Łukasz Domeradzki <JustArchi@JustArchi.net>
2015-04-22 12:42:46 +0200

    Updated sources (Android 5.1.1)

--- system/bin/install-recovery.sh
+++ system/bin/install-recovery.sh
@@ -1,10 +1,10 @@
 #!/system/bin/sh
 if [ -f /system/etc/recovery-transform.sh ]; then
-  exec sh /system/etc/recovery-transform.sh 9402368 db4109982fef549012933e2f1a0dd48df1472ef8 6168576 470606f3d049959bcedb3185c203f9f1ee8505bd
+  exec sh /system/etc/recovery-transform.sh 9402368 c8b7fc3cce3cdb02dcb59bde289259e93d45d286 6168576 0213cb2aef6e4e5972bf19cab1fe5b23a3c7fb56
 fi
 
-if ! applypatch -c EMMC:/dev/block/mmcblk0p6:9402368:db4109982fef549012933e2f1a0dd48df1472ef8; then
-  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/mmcblk0p5:6168576:470606f3d049959bcedb3185c203f9f1ee8505bd EMMC:/dev/block/mmcblk0p6 db4109982fef549012933e2f1a0dd48df1472ef8 9402368 470606f3d049959bcedb3185c203f9f1ee8505bd:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/mmcblk0p6:9402368:c8b7fc3cce3cdb02dcb59bde289259e93d45d286; then
+  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/mmcblk0p5:6168576:0213cb2aef6e4e5972bf19cab1fe5b23a3c7fb56 EMMC:/dev/block/mmcblk0p6 c8b7fc3cce3cdb02dcb59bde289259e93d45d286 9402368 0213cb2aef6e4e5972bf19cab1fe5b23a3c7fb56:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
