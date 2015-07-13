f7015afc3ef2735b67738efa026b8763f4931877
JustArchi <JustArchi@JustArchi.net>
2015-07-13 03:19:49 +0200

    BIG: Switch to OpenGApps provider

---
+++ __build/gapps.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+#     _             _     _ ____            _     _
+#    / \   _ __ ___| |__ (_)  _ \ _ __ ___ (_) __| |
+#   / _ \ | '__/ __| '_ \| | | | | '__/ _ \| |/ _` |
+#  / ___ \| | | (__| | | | | |_| | | | (_) | | (_| |
+# /_/   \_\_|  \___|_| |_|_|____/|_|  \___/|_|\__,_|
+#
+# Copyright 2014-2015 Łukasz "JustArchi" Domeradzki
+# Contact: JustArchi@JustArchi.net
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This script is used to automatically update GApps in AD github tree
+# Big thanks to Open GApps contributors for providing these
+
+set -e
+
+cd "$(dirname "$0")"
+
+while read ZIP; do
+	GAPPSPACKAGE="$ZIP"
+done < <(find .. -mindepth 1 -maxdepth 1 -type f -name "open_gapps-*.zip")
+
+if [[ -z "$GAPPSPACKAGE" ]]; then
+	LATEST="$(curl https://api.github.com/repos/opengapps/opengapps/tags 2>/dev/null | grep "\"name\":" | head -n 1 | cut -d '"' -f 4)"
+	if [[ -z "$LATEST" ]]; then
+		echo "ERROR: Could not fetch last Open GApps tags"
+		exit 1
+	fi
+
+	GAPPSPACKAGE="open_gapps-arm-5.1-stock-$LATEST.zip"
+	wget "https://github.com/opengapps/opengapps/releases/download/$LATEST/$GAPPSPACKAGE"
+fi
+
+rm -rf tmp-gapps
+mkdir tmp-gapps
+mv "$GAPPSPACKAGE" tmp-gapps/
+cd tmp-gapps
+
+unzip "$(basename "$GAPPSPACKAGE")"
+
+# Core files
+rm -rf "../../_archidroid/gapps/base"
+while read TAR; do
+	PACKAGE="$(basename "$TAR" | cut -d '.' -f 1)"
+
+	rm -rf tmp-gapps
+	mkdir tmp-gapps
+	cd tmp-gapps
+
+	tar xf "../$TAR"
+
+	# Common
+	if [[ -d "$PACKAGE/common" ]]; then
+		while read FILE; do
+			cp -R "$FILE" "../../../_archidroid/gapps/base"
+		done < <(find "$PACKAGE/common" -mindepth 1 -maxdepth 1)
+	fi
+
+	# Main
+	if [[ -d "$PACKAGE/nodpi" ]]; then
+		while read FILE; do
+			cp -R "$FILE" "../../../_archidroid/gapps/base"
+		done < <(find "$PACKAGE/nodpi" -mindepth 1 -maxdepth 1)
+	fi
+
+	cd ..
+	rm -rf tmp-gapps
+
+	echo "DONE: $PACKAGE"
+done < <(find Core -mindepth 1 -maxdepth 1 -type f -name "*.tar.xz")
+
+
+# Optional files
+while read TAR; do
+	PACKAGE="$(basename "$TAR" | cut -d '.' -f 1)"
+
+	if [[ ! -d "../../_archidroid/gapps/extra/$PACKAGE" ]]; then
+		echo "INFO: Skipping $PACKAGE"
+		continue
+	fi
+
+	rm -rf tmp-gapps
+	mkdir tmp-gapps
+	cd tmp-gapps
+
+	tar xf "../$TAR"
+
+	rm -rf "../../../_archidroid/gapps/extra/$PACKAGE"
+	mkdir "../../../_archidroid/gapps/extra/$PACKAGE"
+
+	# Common
+	if [[ -d "$PACKAGE/common" ]]; then
+		while read FILE; do
+			cp -R "$FILE" "../../../_archidroid/gapps/extra/$PACKAGE"
+		done < <(find "$PACKAGE/common" -mindepth 1 -maxdepth 1)
+	fi
+
+	# Main
+	if [[ -d "$PACKAGE/nodpi" ]]; then
+		while read FILE; do
+			cp -R "$FILE" "../../../_archidroid/gapps/extra/$PACKAGE"
+		done < <(find "$PACKAGE/nodpi" -mindepth 1 -maxdepth 1)
+	fi
+
+	cd ..
+	rm -rf tmp-gapps
+
+	echo "DONE: $PACKAGE"
+done < <(find GApps -mindepth 1 -maxdepth 1 -type f -name "*.tar.xz")
+
+cd ..
+rm -rf tmp-gapps
+
+exit 0
