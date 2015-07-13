917969ac02dba980e06d8137e48d16abce2faa5c
JustArchi <JustArchi@JustArchi.net>
2015-07-13 04:39:15 +0200

    bcm4334: Correct firmware location
    Commit https://github.com/ArchiDroid/android_device_samsung_smdk4412-common/commit/68000c2b1d3fb182de252c06c9c63dee169fc1fb introduced new firmware location, which BCM tuning must be able to handle
    Also, cleanup these scripts once we're here.

--- META-INF/com/google/android/aroma/exec/bcm4334.sh
+++ META-INF/com/google/android/aroma/exec/bcm4334.sh
@@ -21,65 +21,66 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Detects BCM4334 manufacturer
+# String is returned on success
+
+set -e
+
 BCM="Unknown"
 SUCCESS=0
 
-DETECT_BCM() {
-	if [[ -f "/data/.cid.info" ]]; then
-		BCM="$(cat "/data/.cid.info")"
-		if [[ -n "$BCM" ]]; then
-			SUCCESS=1
-			return
-		fi
-	fi
-	if [[ -f "/efs/wifi/.mac.info" ]]; then
+if [[ -f "/data/.cid.info" ]]; then
+	BCMPROBE="$(cat "/data/.cid.info")"
+	if [[ -n "$BCMPROBE" ]]; then
+		BCM="$BCMPROBE"
 		SUCCESS=1
-		BCM="$(tr '[:upper:]' '[:lower:]' < "/efs/wifi/.mac.info")"
-		case "$BCM" in # Ref: android_hardware_samsung -> macloader/macloader.cpp
-			# murata
-			"00:0e:6d"*) BCM="murata" ;;
-			"00:13:e0"*) BCM="murata" ;;
-			"00:21:e8"*) BCM="murata" ;;
-			"00:26:e8"*) BCM="murata" ;;
-			"00:37:6d"*) BCM="murata" ;;
-			"00:60:57"*) BCM="murata" ;;
-			"04:46:65"*) BCM="murata" ;;
-			"10:5f:06"*) BCM="murata" ;;
-			"10:a5:d0"*) BCM="murata" ;;
-			"1c:99:4c"*) BCM="murata" ;;
-			"14:7d:c5"*) BCM="murata" ;;
-			"20:02:af"*) BCM="murata" ;;
-			"40:f3:08"*) BCM="murata" ;;
-			"44:a7:cf"*) BCM="murata" ;;
-			"5c:da:d4"*) BCM="murata" ;;
-			"5c:f8:a1"*) BCM="murata" ;;
-			"78:4b:87"*) BCM="murata" ;;
-			"60:21:c0"*) BCM="murata" ;;
-			"88:30:8a"*) BCM="murata" ;;
-			"f0:27:65"*) BCM="murata" ;;
-
-			# semco3rd
-			"f4:09:d8"*) BCM="semco3rd" ;;
-
-			# semcosh
-			"34:23:ba"*) BCM="semcosh" ;;
-			"38:aa:3c"*) BCM="semcosh" ;;
-			"50:cc:f8"*) BCM="semcosh" ;;
-			"5c:0a:5b"*) BCM="semcosh" ;;
-			"88:32:9b"*) BCM="semcosh" ;;
-			"90:18:7c"*) BCM="semcosh" ;;
-			"cc:3a:61"*) BCM="semcosh" ;;
-
-			# wisol
-			"48:5a:3f"*) BCM="wisol" ;;
-
-			# Unknown
-			*) SUCCESS=0; BCM="Unknown, $BCM" ;;
-		esac
 	fi
-}
+fi
+if [[ "$SUCCESS" -eq 0 && -f "/efs/wifi/.mac.info" ]]; then
+	SUCCESS=1
+	MAC="$(tr '[:upper:]' '[:lower:]' < "/efs/wifi/.mac.info")"
+	case "$MAC" in # Ref: android_hardware_samsung -> macloader/macloader.cpp
+		# murata
+		"00:0e:6d"*) BCM="murata" ;;
+		"00:13:e0"*) BCM="murata" ;;
+		"00:21:e8"*) BCM="murata" ;;
+		"00:26:e8"*) BCM="murata" ;;
+		"00:37:6d"*) BCM="murata" ;;
+		"00:60:57"*) BCM="murata" ;;
+		"04:46:65"*) BCM="murata" ;;
+		"10:5f:06"*) BCM="murata" ;;
+		"10:a5:d0"*) BCM="murata" ;;
+		"1c:99:4c"*) BCM="murata" ;;
+		"14:7d:c5"*) BCM="murata" ;;
+		"20:02:af"*) BCM="murata" ;;
+		"40:f3:08"*) BCM="murata" ;;
+		"44:a7:cf"*) BCM="murata" ;;
+		"5c:da:d4"*) BCM="murata" ;;
+		"5c:f8:a1"*) BCM="murata" ;;
+		"78:4b:87"*) BCM="murata" ;;
+		"60:21:c0"*) BCM="murata" ;;
+		"88:30:8a"*) BCM="murata" ;;
+		"f0:27:65"*) BCM="murata" ;;
 
-DETECT_BCM
+		# semco3rd
+		"f4:09:d8"*) BCM="semco3rd" ;;
+
+		# semcosh
+		"34:23:ba"*) BCM="semcosh" ;;
+		"38:aa:3c"*) BCM="semcosh" ;;
+		"50:cc:f8"*) BCM="semcosh" ;;
+		"5c:0a:5b"*) BCM="semcosh" ;;
+		"88:32:9b"*) BCM="semcosh" ;;
+		"90:18:7c"*) BCM="semcosh" ;;
+		"cc:3a:61"*) BCM="semcosh" ;;
+
+		# wisol
+		"48:5a:3f"*) BCM="wisol" ;;
+
+		# Unknown
+		*) SUCCESS=0; BCM="Unknown, $MAC"
+	esac
+fi
 
 if [[ "$SUCCESS" -eq 1 ]]; then
 	echo "$BCM" > "/tmp/archidroid/bcm4334"
--- _archidroid/exec/bcm4334.sh
+++ _archidroid/exec/bcm4334.sh
@@ -6,7 +6,7 @@
 #  / ___ \| | | (__| | | | | |_| | | | (_) | | (_| |
 # /_/   \_\_|  \___|_| |_|_|____/|_|  \___/|_|\__,_|
 #
-# Copyright 2014 Łukasz "JustArchi" Domeradzki
+# Copyright 2014-2015 Łukasz "JustArchi" Domeradzki
 # Contact: JustArchi@JustArchi.net
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -21,28 +21,47 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Sets proper firmware as default fallback
+# $1 - Target firmware. Can be either: stock, archidetect (requires detection firstly) or any valid string from bcm4334_*.hcd
+
+set -e
+
+if [[ -z "$1" ]]; then
+	exit 1
+fi
+
 BCM="$1"
+
 case "$BCM" in
 	"stock") exit 0 ;;
 	"archidetect")
-			if [[ -f "/tmp/archidroid/bcm4334" ]]; then
-				BCM="$(cat "/tmp/archidroid/bcm4334")"
-				rm -f "/tmp/archidroid/bcm4334"
-			else
-				exit 0
-			fi
-			;;
+		if [[ -f "/tmp/archidroid/bcm4334" ]]; then
+			BCM="$(cat "/tmp/archidroid/bcm4334")"
+			rm -f "/tmp/archidroid/bcm4334"
+		else
+			exit 1
+		fi
+	;;
 esac
 
-if [[ ! -z "$BCM" && -f "/system/bin/bcm4334.hcd" && -f "/system/bin/bcm4334_$BCM.hcd" ]]; then
-	cp -p "/system/bin/bcm4334_$BCM.hcd" "/system/bin/bcm4334.hcd"
+# Detect BCM firmware location
+if [[ -f "/system/bin/bcm4334.hcd" ]]; then
+	FWPATH="/system/bin"
+elif [[ -f "/system/vendor/firmware/bluetooth/bcm4334.hcd" ]]; then
+	FWPATH="/system/vendor/firmware/bluetooth"
+else
+	exit 1
+fi
+
+if [[ -f "$FWPATH/bcm4334_$BCM.hcd" ]]; then
+	cp -p "$FWPATH/bcm4334_$BCM.hcd" "$FWPATH/bcm4334.hcd"
 	if [[ ! -f "/data/.cid.info" ]]; then
-		printf "$BCM" > "/data/.cid.info"
+		printf "%s" "$BCM" > "/data/.cid.info"
 		chmod 666 "/data/.cid.info"
 		chown 1000:1000 "/data/.cid.info"
 	fi
 else
-	exit 0
+	exit 1
 fi
 
 exit 0
