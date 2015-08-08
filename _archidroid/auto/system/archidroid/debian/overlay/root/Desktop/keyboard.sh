2cb7feeb522101d4aed7b57411938050228e2576
JustArchi <JustArchi@JustArchi.net>
2015-08-08 17:17:51 +0200

    Add --xorg to Pocket Debian This is still big work in progress and may contain countless bugs, use at your own risk

---
+++ _archidroid/auto/system/archidroid/debian/overlay/root/Desktop/keyboard.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#     _             _     _ ____            _     _
+#    / \   _ __ ___| |__ (_)  _ \ _ __ ___ (_) __| |
+#   / _ \ | '__/ __| '_ \| | | | | '__/ _ \| |/ _` |
+#  / ___ \| | | (__| | | | | |_| | | | (_) | | (_| |
+# /_/   \_\_|  \___|_| |_|_|____/|_|  \___/|_|\__,_|
+#
+# Copyright 2014-2015 Łukasz "JustArchi" Domeradzki
+# Contact: JustArchi@JustArchi.net
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -eu
+
+# Exit if stdout is TTY
+if [[ -t 1 ]]; then
+	xmessage -center -font -bitstream-terminal-bold-r-normal--20-140-100-100-c-110-iso8859-1 "You can't execute this script in terminal!"
+	exit 1
+fi
+
+xvkbd >/dev/null 2>&1 &
