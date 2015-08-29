9021dc2e22c5f3313837d4bed9e490866da58408
JustArchi <JustArchi@JustArchi.net>
2015-08-29 10:59:19 +0200

    Updated sources

--- system/bin/install-recovery.sh
+++ system/bin/install-recovery.sh
@@ -1,10 +1,10 @@
 #!/system/bin/sh
 if [ -f /system/etc/recovery-transform.sh ]; then
-  exec sh /system/etc/recovery-transform.sh 11767808 b9069bc3c68089215ec1a83d54c8b237acee151b 13803520 f86698d3a82c424293c5360067807c41d04eb897
+  exec sh /system/etc/recovery-transform.sh 11767808 6f7ba77ad454e3027a9feaf81e3b6015bfea9c4e 13803520 b66eb7b42fdb561d13bb52a8692f53c93f117cda
 fi
 
-if ! applypatch -c EMMC:/dev/block/platform/msm_sdcc.1/by-name/FOTAKernel:11767808:b9069bc3c68089215ec1a83d54c8b237acee151b; then
-  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/platform/msm_sdcc.1/by-name/boot:13803520:f86698d3a82c424293c5360067807c41d04eb897 EMMC:/dev/block/platform/msm_sdcc.1/by-name/FOTAKernel b9069bc3c68089215ec1a83d54c8b237acee151b 11767808 f86698d3a82c424293c5360067807c41d04eb897:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/platform/msm_sdcc.1/by-name/FOTAKernel:11767808:6f7ba77ad454e3027a9feaf81e3b6015bfea9c4e; then
+  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/platform/msm_sdcc.1/by-name/boot:13803520:b66eb7b42fdb561d13bb52a8692f53c93f117cda EMMC:/dev/block/platform/msm_sdcc.1/by-name/FOTAKernel 6f7ba77ad454e3027a9feaf81e3b6015bfea9c4e 11767808 b66eb7b42fdb561d13bb52a8692f53c93f117cda:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
