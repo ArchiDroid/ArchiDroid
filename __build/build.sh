31014847fac11005145603bcd4e43852a052e374
JustArchi <JustArchi@JustArchi.net>
2016-01-31 14:17:33 +0100

    Alter structure

--- __build/build.sh
+++ __build/build.sh
@@ -6,7 +6,7 @@
 #  / ___ \| | | (__| | | | | |_| | | | (_) | | (_| |
 # /_/   \_\_|  \___|_| |_|_|____/|_|  \___/|_|\__,_|
 #
-# Copyright 2014 Łukasz "JustArchi" Domeradzki
+# Copyright 2014-2016 Łukasz "JustArchi" Domeradzki
 # Contact: JustArchi@JustArchi.net
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -23,11 +23,12 @@
 
 set -eu
 
+cd "$(dirname "$0")"
+
 VERSION=3.2
 
-# Device-based
-DEVICE="BRINGUP" # This is AOSP variant to build, the one used in brunch command. If you use "brunch i9300", you should set it to i9300 here
-BUILDVARIANT="userdebug" # Change this to user if you feel brave
+# Load device properties
+source device.sh
 
 # Rom-based
 ROM="CyanogenMod" # This is actually for information purpose only, can be anything
@@ -36,17 +37,18 @@ CLOSED_SOURCE=0 # If 1, build.sh won't try to build it from source, but use preb
 
 # Detect HOME properly
 # This workaround is required because arm-eabi-nm has problems following ~. Don't change it
-if [[ "$(dirname ~)" = "/" ]]; then
-	HOME="$(dirname ~)$(basename ~)" # Root
+HOME="$(dirname ~)"
+if [[ "$HOME" = "/" ]]; then
+	HOME="/$(basename ~)" # Root
 else
-	HOME="$(dirname ~)/$(basename ~)" # User
+	HOME="$HOME/$(basename ~)" # User
 fi
 
 # Try to not change these if you can
 ADROOT="$HOME/shared/git/ArchiDroid" # This is where ArchiDroid GitHub repo is located
 ADZIP="$ROMSHORT-*.zip" # This is with what defined output zip. For omni it would be "omni-*.zip"
 ADCOMPILEROOT="$HOME/android/$ROMSHORT" # This is where AOSP sources are located
-ADOUT="$ADCOMPILEROOT/out/target/product/$DEVICE" # This is the location of output zip from above sources, usually it doesn't need to be changed
+ADOUT="$ADCOMPILEROOT/out/target/product/$DEVICE_CODENAME" # This is the location of output zip from above sources, usually it doesn't need to be changed
 ADSMPREBUILTS="$HOME/sabermod-prebuilts" # A directory which should contain SaberMod prebuilts from http://sabermod.com which are used during ROM compiling
 JOBS="$(grep -c "processor" "/proc/cpuinfo")" # Maximum number of jobs, can be declared statically if needed, default to number of threads of the CPU
 
@@ -110,8 +112,6 @@ STABLE=0
 STOCK="$CLOSED_SOURCE"
 OLD=0
 SYNC=0
-
-cd "$(dirname "$0")"
 
 for ARG in "$@" ; do
 	case "$ARG" in
@@ -211,12 +211,12 @@ if [[ "$PREBUILT" -eq 0 ]]; then
 	fi
 
 	if [[ "$RECOVERY" -eq 1 ]]; then
-		lunch "cm_$DEVICE-$BUILDVARIANT"
+		lunch "cm_$DEVICE_CODENAME-$DEVICE_CODENAME_BUILDVARIANT"
 		make -j "$JOBS" recoveryimage
 		exit 0
 	fi
 
-	brunch "$DEVICE" "$BUILDVARIANT" || true
+	brunch "$DEVICE_CODENAME" "$DEVICE_CODENAME_BUILDVARIANT" || true
 
 	REALADZIP=""
 	while read ZIP; do
@@ -287,7 +287,7 @@ if [[ "$OLDMD5" != "$NEWMD5" ]]; then
 elif [[ ! -f "__build/_updater-scripts/archidroid/updater-script" ]]; then
 	mkdir -p __build/_updater-scripts/archidroid
 	cp META-INF/com/google/android/updater-script __build/_updater-scripts/archidroid/updater-script
-	echo "WARNING: This is your FIRST build for unsupported device"
+	echo "WARNING: This is your FIRST build for unsupported DEVICE_CODENAME"
 	echo "Congratulations for going THAT far!"
 	echo "Don't forget to add permissions and symlinks to ArchiDroid's updater-script"
 	echo "Search for 'BRINGUP' word in META-INF/com/google/android/updater-script"
@@ -377,7 +377,7 @@ fi
 {
 	echo "# ArchiDroid build.prop"
 	echo "ro.archidroid=1"
-	echo "ro.archidroid.device=$DEVICE"
+	echo "ro.archidroid.DEVICE_CODENAME=$DEVICE_CODENAME"
 	echo "ro.archidroid.rom=$ROM"
 	echo "ro.archidroid.rom.short=$ROMSHORT"
 	echo "ro.archidroid.version=$VERSION"
@@ -387,7 +387,7 @@ fi
 } > /tmp/build.prop.TEMP
 
 # Change default version to our custom one
-sed -i "s/ro.build.display.id=.*/ro.build.display.id=ArchiDroid-$VERSION-$TVERSION-$AVERSION-$ROM-$DEVICE/g" /tmp/build.prop.TEMP
+sed -i "s/ro.build.display.id=.*/ro.build.display.id=ArchiDroid-$VERSION-$TVERSION-$AVERSION-$ROM-$DEVICE_CODENAME/g" /tmp/build.prop.TEMP
 
 # Enable root by default. User has a choice in AROMA
 sed -i "s/persist.sys.root_access=.*/persist.sys.root_access=1/g" /tmp/build.prop.TEMP
---
+++ __build/device.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#     _             _     _ ____            _     _
+#    / \   _ __ ___| |__ (_)  _ \ _ __ ___ (_) __| |
+#   / _ \ | '__/ __| '_ \| | | | | '__/ _ \| |/ _` |
+#  / ___ \| | | (__| | | | | |_| | | | (_) | | (_| |
+# /_/   \_\_|  \___|_| |_|_|____/|_|  \___/|_|\__,_|
+#
+# Copyright 2014-2016 Łukasz "JustArchi" Domeradzki
+# Contact: JustArchi@JustArchi.net
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This script should define device-based properties
+
+DEVICE_CODENAME="BRINGUP" # This is AOSP variant to build, the one used in brunch command. If you use "brunch i9300", you should set it to i9300
+DEVICE_BUILDVARIANT="userdebug" # "user" is best, but requires extra fixes, so "userdebug" is default
+
+# Apart from above, don't forget to:
+# 1. Copy permission and symlinks to updater-script after first build
+# 2. Copy appropriate overlay from "_devices" directory
+# 3. Set device properties also in aroma-config
+# 4. Put proper GAPPS using gapps.sh
+# 5. I most likely forgot about something anyway, will update as needed
+
+exit 0
