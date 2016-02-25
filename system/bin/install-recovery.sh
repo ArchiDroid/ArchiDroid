322234feb26d807a70f486e249d500f825efb8a2
JustArchi <JustArchi@JustArchi.net>
2016-02-25 22:08:28 +0100

    Updated sources

--- system/bin/install-recovery.sh
+++ system/bin/install-recovery.sh
@@ -1,10 +1,10 @@
 #!/system/bin/sh
 if [ -f /system/etc/recovery-transform.sh ]; then
-  exec sh /system/etc/recovery-transform.sh 30588928 3f4269eb4363b4ff29b25a3592fcaf635503768a 27103232 329cdfccf09f3e720d43af4c4b316d05aa1189dc
+  exec sh /system/etc/recovery-transform.sh 30392320 11c6f585e3fda6bfa818139730163b84a1ac2b00 26906624 74c0a6b217db00a484ba96b20255a20b24494a54
 fi
 
-if ! applypatch -c EMMC:/dev/block/bootdevice/by-name/recovery:30588928:3f4269eb4363b4ff29b25a3592fcaf635503768a; then
-  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/bootdevice/by-name/boot:27103232:329cdfccf09f3e720d43af4c4b316d05aa1189dc EMMC:/dev/block/bootdevice/by-name/recovery 3f4269eb4363b4ff29b25a3592fcaf635503768a 30588928 329cdfccf09f3e720d43af4c4b316d05aa1189dc:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/bootdevice/by-name/recovery:30392320:11c6f585e3fda6bfa818139730163b84a1ac2b00; then
+  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/bootdevice/by-name/boot:26906624:74c0a6b217db00a484ba96b20255a20b24494a54 EMMC:/dev/block/bootdevice/by-name/recovery 11c6f585e3fda6bfa818139730163b84a1ac2b00 30392320 74c0a6b217db00a484ba96b20255a20b24494a54:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
